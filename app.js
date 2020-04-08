f3db1adcdca0c1084b3c51ebbfff329374b235da
Shivankar Bansal <shivankarbansal27@gmail.com>
2020-04-08 08:15:54 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,96 @@
+var express = require("express"),
+
+    app = express(),
+
+    bodyParser = require("body-parser"),
+
+    mongoose = require("mongoose"),
+    flash = require('connect-flash'),
+    Campground = require("./models/campground"),
+    seedDB = require("./seeds"),
+    Comment = require("./models/comment"),
+    passport = require('passport'),
+    LocalStrategy = require('passport-local'),
+    passportLocalMongoose = require('passport-local-mongoose'),
+    User = require("./models/user"),
+    methodOverride = require("method-override")
+var commentRoutes = require("./routes/comments"),
+    campgroundRoutes = require("./routes/campgrounds"),
+    indexRoutes = require("./routes/index")
+var port = process.env.PORT || 8086
+    //seedDB();
+
+mongoose.connect("mongodb://localhost/yelp_camp");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + "/public"));
+app.use(methodOverride("_method"));
+app.use(flash());
+
+app.set("view engine", "ejs");
+//PASSPORT CONFIGURATION
+app.use(require("express-session")({
+    secret: "Once again Rusty wins the cutest dog",
+    resave: false,
+    saveUninitialized: false
+}));
+passport.use(new LocalStrategy(User.authenticate()));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(bodyParser.urlencoded({ extended: true }))
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+app.use(function(req, res, next) {
+    res.locals.currentUser = req.user;
+    res.locals.error = req.flash("error");
+    res.locals.success = req.flash("success");
+    next();
+});
+
+
+
+// SCHEMA SETUP
+
+
+
+
+// Campground.create(
+
+//      {
+
+//          name: "Granite Hill", 
+
+//          image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg",
+
+//          description: "This is a huge granite hill, no bathrooms.  No water. Beautiful granite!"
+
+
+
+//      },
+
+//      function(err, campground){
+
+//       if(err){
+
+//           console.log(err);
+
+//       } else {
+
+//           console.log("NEWLY CREATED CAMPGROUND: ");
+
+//           console.log(campground);
+
+//       }
+
+//     });
+
+
+
+app.use("/", indexRoutes);
+app.use("/campgrounds", campgroundRoutes);
+app.use("/campgrounds/:id/comments", commentRoutes);
+
+
+app.listen(port, function() {
+    console.log("Yelpcamp about to start");
+});
---
+++ middleware/index.js
@@ -0,0 +1,52 @@
+//all the middleware goes here
+var Campground = require("../models/campground");
+var Comment = require("../models/comment");
+var middlewareObj = {}
+middlewareObj.checkCampgroundOwnership = function(req, res, next) {
+    //does the user own campground
+    if (req.isAuthenticated()) {
+        Campground.findById(req.params.id, function(err, foundCampground) {
+            if (err) {
+                req.flash("error", "Campground not found")
+                res.redirect("back");
+            } else {
+                if (foundCampground.author.id.equals(req.user._id)) {
+                    next();
+                } else {
+                    req.flash("error", "You dont have permission to do that");
+                    res.redirect("back");
+                }
+            }
+        });
+    } else {
+        req.flash("error", "You need to be logged in");
+        res.redirect("back");
+    }
+
+}
+middlewareObj.checkCommentOwnership = function(req, res, next) {
+    if (req.isAuthenticated()) {
+        Comment.findById(req.params.comment_id, function(err, foundComment) {
+            if (err) {
+                res.redirect("back");
+            } else {
+                //does the user own campground
+                if (foundComment.author.id.equals(req.user._id)) {
+                    next();
+                } else {
+                    res.redirect("back");
+                }
+            }
+        });
+    } else {
+        res.redirect("back");
+    }
+}
+middlewareObj.isLoggedIn = function(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    req.flash("error", "Please login first");
+    res.redirect("/login");
+}
+module.exports = middlewareObj;
