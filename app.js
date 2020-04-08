e0889b256295cf9cb89c0e268789cca71925cf9b
Shivankar Bansal <shivankarbansal27@gmail.com>
2020-04-08 09:56:59 +0530

    mongo add

--- app.js
+++ app.js
@@ -19,11 +19,8 @@ var commentRoutes = require("./routes/comments"),
     indexRoutes = require("./routes/index")
 var port = process.env.PORT || 8086
     //seedDB();
+mongoose.connect(process.env.DATABASEURL);
 
-mongoose.connect("mongodb+srv://devd:6Hiv@nker@cluster0-bsvef.mongodb.net/test?retryWrites=true&w=majority", {
-    useNewUrlParser: true,
-    useCreateIndex: true
-});
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/public"));
